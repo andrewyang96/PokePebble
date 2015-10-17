ad92ea99d66325cc65ee914004cf1a8bae251b80
Andrew Yang <andrewyang96@users.noreply.github.com>
2015-10-17 15:35:37 -0400

    Try to do battle UI

--- src/app.js
+++ src/app.js
@@ -23,9 +23,77 @@ title.show();
 var battleWind = new UI.Window({
   fullscreen: true
 });
-var drawBattleWind = function (state) {
-  // TODO
-};
+
+var yourHP = new UI.Rect({
+  size: new Vector2(52, 5),
+  position: new Vector2(72, 120),
+  borderColor: 'black',
+  backgroundColor: 'white'
+});
+battleWind.add(yourHP);
+var yourHPBar = new UI.Rect({
+  size: new Vector2(50, 3),
+  position: new Vector2(73, 121),
+  borderColor: 'black',
+  backgroundColor: 'black'
+}); // TODO: make dynamic
+battleWind.add(yourHPBar);
+var yourPokeName = new UI.Text({
+  text: "YOURPOKE",
+  font: 'gothic-18-bold',
+  color: 'black',
+  textOverflow: 'ellipsis',
+  textAlign: 'left',
+  position: new Vector2(73, 81),
+  size: new Vector2(50, 25)
+});
+battleWind.add(yourPokeName);
+var yourPokeInfo = new UI.Text({
+  text: "L69, M, BRN",
+  font: 'gothic-14',
+  color: 'black',
+  textOverflow: 'wrap',
+  textAlign: 'left',
+  position: new Vector2(73, 106),
+  size: new Vector2(50, 15)
+});
+battleWind.add(yourPokeInfo);
+
+var opponentHP = new UI.Rect({
+  size: new Vector2(52, 5),
+  position: new Vector2(20, 64),
+  borderColor: 'black',
+  backgroundColor: 'white'
+});
+battleWind.add(opponentHP);
+var opponentHPBar = new UI.Rect({
+  size: new Vector2(50, 3),
+  position: new Vector2(21, 65),
+  borderColor: 'black',
+  backgroundColor: 'black'
+});
+battleWind.add(opponentHPBar);
+var opponentPokeName = new UI.Text({
+  text: "OPPONENT",
+  font: 'gothic-18-bold',
+  color: 'black',
+  textOverflow: 'ellipsis',
+  textAlign: 'left',
+  position: new Vector2(20, 24),
+  size: new Vector2(50, 25)
+});
+battleWind.add(opponentPokeName);
+var opponentPokeInfo = new UI.Text({
+  text: "L81, PAR",
+  font: 'gothic-14',
+  color: 'black',
+  textOverflow: 'wrap',
+  textAlign: 'left',
+  position: new Vector2(20, 49),
+  size: new Vector2(50, 15)
+});
+battleWind.add(opponentPokeInfo);
+
 var clearRect = new UI.Rect({
   size: new Vector2(144, 168)
 });
@@ -39,13 +107,15 @@ var infoRect = new UI.Rect({
 battleWind.add(infoRect);
 var infoText = new UI.Text({
   text: "The battle has started. Press SELECT",
-  font: 'gothic-14-bold',
+  font: 'gothic-14',
   color: 'black',
   textOverflow: 'wrap',
   textAlign: 'left',
-  position: new Vector2(0, 144)
+  position: new Vector2(0, 144),
+  size: new Vector2(144, 24)
 });
 battleWind.add(infoText);
+
 var textQueue = [];
 var displayingText = false;
 var displayText = function (msg) {
