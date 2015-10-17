442546d69b090624609d681bf7b6547324c21e7a
Andrew Yang <andrewyang96@users.noreply.github.com>
2015-10-17 00:09:51 -0500

    Select button to search for opponent

--- src/app.js
+++ src/app.js
@@ -5,55 +5,25 @@
  */
 
 var UI = require('ui');
-var Vector2 = require('vector2');
 
-var main = new UI.Card({
+var title = new UI.Card({
   title: 'PokePebble',
   banner: 'images/LogoPS.png',
-  body: 'A Pebble client for Pokemon showdown.'
+  body: 'A Pebble client for Pokemon showdown. Press SELECT to begin.'
 });
 
-main.show();
+title.show();
 
-main.on('click', 'up', function(e) {
-  var menu = new UI.Menu({
-    sections: [{
-      items: [{
-        title: 'Pebble.js',
-        icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus'
-      }, {
-        title: 'Second Item',
-        subtitle: 'Subtitle Text'
-      }]
-    }]
-  });
-  menu.on('select', function(e) {
-    console.log('Selected item #' + e.itemIndex + ' of section #' + e.sectionIndex);
-    console.log('The item is titled "' + e.item.title + '"');
-  });
-  menu.show();
-});
+// TODO: Log in
 
-main.on('click', 'select', function(e) {
-  var wind = new UI.Window({
-    fullscreen: true,
+title.on('click', 'select', function (e) {
+  var searching = new UI.Card();
+  searching.title('Searching for an opponent.');
+  searching.body('Press BACK to cancel.');
+  searching.show();
+  // TODO: Search for an opponent
+  searching.on('click', 'back', function (e) {
+    // TODO: cancel search
+    title.show();
   });
-  var textfield = new UI.Text({
-    position: new Vector2(0, 65),
-    size: new Vector2(144, 30),
-    font: 'gothic-24-bold',
-    text: 'Text Anywhere!',
-    textAlign: 'center'
-  });
-  wind.add(textfield);
-  wind.show();
-});
-
-main.on('click', 'down', function(e) {
-  var card = new UI.Card();
-  card.title('A Card');
-  card.subtitle('Is a Window');
-  card.body('The simplest window type in Pebble.js.');
-  card.show();
 });
